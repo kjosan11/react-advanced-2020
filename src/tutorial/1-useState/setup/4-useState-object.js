89c32e1246976fdb60992681a5d8ddb28276496b
kjosan11 <josank3311@gmail.com>
2020-12-02 15:27:45 -0800

    finished useState, useEffect, & conditional rendering

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 
+// another way of declaring objects 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  //object 
+  //const [person, setpPerson] = useState({name: 'peter', age: '24', message: 'randome message'});
+  
+  // declaring function changeMessage 
+  // ...person is a spread operator -> which helps
+  // keep the person's name and age and only change the message 
+
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState('24')
+  const [message, setMessage] = useState('random message')
+  const changeMessage = () => {
+    //setpPerson({...person, message: 'hello world'});
+    setMessage('hello world');
+  }
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>change message</button>
+   </>
+  );
 };
 
 export default UseStateObject;
